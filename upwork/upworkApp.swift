4045311d7e466bd2171f9e386d403172e2390351
Gatien DIDRY <gatien.didry@orange.fr>
2023-01-06 09:13:38 +0100

    Initial Commit

---
+++ upwork/upworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  upworkApp.swift
+//  upwork
+//
+//  Created by Gatien DIDRY on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct upworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
