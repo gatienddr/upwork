dbb0ed6128ad71c4f62e2dc21dc73c31d0b09a9a
louiscauret <louis.cauret@ynov.com>
2023-01-08 17:08:01 +0100

    Revert "WeekDataView"

--- upwork/views/weekData/WeekDataViewModel.swift
+++ upwork/views/weekData/WeekDataViewModel.swift
@@ -2,13 +2,10 @@
 //  WeekDataViewModel.swift
 //  upwork
 //
-//  Created by Louis Cauret on 06/01/2023.
+//  Created by Gatien DIDRY on 06/01/2023.
 //
 
 import Foundation
-import SwiftUI
 
-class WeekDataViewModel: ObservableObject {
-    
+class WeekDataViewModel {
 }
-
