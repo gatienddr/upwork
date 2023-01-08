ebbe841da98d0f9dc984b0a16b248205135cdce0
Louis Cauret <louiscauret@gmail.com>
2023-01-08 13:04:59 +0100

    WeekDataView
    Creating new view to show all infos about employees

---
+++ upwork/models/WeekData.swift
@@ -0,0 +1,16 @@
+//
+//  WeekData.swift
+//  upwork
+//
+//  Created by Louis Cauret on 07/01/2023.
+//
+
+import Foundation
+
+struct WeekData {
+    var numberHoursWorked: Int
+    var numberOfEmployees: Int
+    var numberHoursPerEmployees: Int
+    var numberOfEmployeesOut: Int
+    var numberOfHoursPerDay: Int
+}
--- upwork/views/weekData/WeekDataView.swift
+++ upwork/views/weekData/WeekDataView.swift
@@ -2,20 +2,39 @@
 //  WeekDataView.swift
 //  upwork
 //
-//  Created by Gatien DIDRY on 06/01/2023.
+//  Created by Louis Cauret on 06/01/2023.
 //
 
 import SwiftUI
 
 struct WeekDataView: View {
-
+    
     private var viewModel: WeekDataViewModel
-
+    
     init(viewModel: WeekDataViewModel) {
         self.viewModel = viewModel
     }
-
+    
     var body: some View {
-        Text("Week data")
+        ScrollView {
+            Text("Cette semaine")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            VStack {
+                HoursCard(image: "working-time", hours: 1250)
+                HStack {
+                    EmployeesCard(image: "division", numberEmployees: 5)
+                    HoursPerEmployeeCard(image: "diagram", hours: 35)
+                }
+                HStack {
+                    EmployeeInVacationCard(image: "vacation", numberOfEmployeeInVacation: 2)
+                    NumberOFHoursPerDayCard(image: "clock", hours: 210)
+                }
+            }
+        }
+        .padding(.all, 20)
     }
 }
